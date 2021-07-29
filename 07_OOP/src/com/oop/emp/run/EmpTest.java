a094d5e979abfa91cb072e07672608db0d3f4354
gksqu <gksqu@LAPTOP-QR5M5A6J>
2021-07-29 13:54:47 +0900

    수정메뉴 추가

--- 07_OOP/src/com/oop/emp/run/EmpTest.java
+++ 07_OOP/src/com/oop/emp/run/EmpTest.java
@@ -8,7 +8,7 @@ public class EmpTest {
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		Employee ep = new Employee();
+		Employee ep = null;
 		int num;
 		abc:
 		while(true) {
@@ -17,15 +17,24 @@ public class EmpTest {
 		
 			switch(num) {
 			case 1:
+				ep = new Employee();
 				ep.empInput();
 				break;
 			case 2:
-				ep.delete();
+				modifyuMenu(ep);
 				break;
 			case 3:
+				ep = null;
+				break;
+			case 4:
+				if(ep == null) {
+					System.out.println(">>먼저 사원정보를 입력하세요.");
+					break;
+				}	
 				ep.empOutput();
 				break;
 			case 9:
+				ep = null;
 				break abc;
 				
 			}
@@ -39,11 +48,54 @@ public class EmpTest {
 		
 		System.out.println("******* 사원 정보 관리 프로그램 ************");
 		System.out.println("1. 새 사원 정보 입력");
-		System.out.println("2. 사원 정보 삭제");
-		System.out.println("3. 사원정보 출력");
+		System.out.println("2. 사원 정보 수정");
+		System.out.println("3. 사원 정보 삭제");
+		System.out.println("4. 사원정보 출력");
 		System.out.println("9. 끝내기");
 		System.out.println("********************************************");
 		
+	}
+	
+	public static void modifyuMenu(Employee ep) {
+		int num;
+		int any2;
+		String any;
+		String modifyuMenu = "********** 사원 정보 수정 메뉴 **********\r\n"
+				+ "	1. 이름 변경 => setEmpName()\r\n"
+				+ "	2. 급여 변경 => setSalary()\r\n"
+				+ "	3. 부서 변경 => setDept()\r\n"
+				+ "	4. 직급 변경 => setJob()\r\n"
+				+ "	5. 이전 메뉴로 이동 => return 처리할 것\r\n"
+				+ "********************************************";
+		while(true) {
+		System.out.print(modifyuMenu);
+		num = sc.nextInt();
+		switch(num) {
+		case 1:
+			System.out.print("이름 변경 : ");
+			any = sc.next();
+			ep.setEmpName(any);
+			break;
+		case 2:
+			System.out.print("급여 변경 : ");
+			any2 = sc.nextInt();
+			ep.setSalary(any2);
+			break;
+		case 3:
+			System.out.print("부서 변경 : ");
+			any = sc.next();
+			ep.setDept(any);
+			break;
+		case 4:
+			System.out.print("직급 변경 : ");
+			any = sc.next();
+			ep.setJob(any);
+			break;
+		case 5:
+			return;
+			}
+	
+		}
 	}
 
 }
